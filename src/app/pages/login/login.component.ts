7e8ba71efcc59022891303e31ccd88df16d3e7bf
Caio Domingos <caiod.dev@gmail.com>
2023-01-30 22:05:09 -0300

    login form design

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  loginForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.createForm();
+  }
+
+  private createForm() {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  getErrorMessage(control: string) {
+    return this.loginForm.controls[control].hasError('required')
+      ? 'Precisa inserir um valor válido'
+      : this.loginForm.controls[control].hasError('email')
+      ? 'Não é um email válido'
+      : this.loginForm.controls[control].hasError('minlength')
+      ? 'Mínimo de 6 caracteres'
+      : this.loginForm.controls[control].hasError('mask')
+      ? 'Formato de campo não válido'
+      : '';
+  }
+
+  login() {}
 }
--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -3,15 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
-
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MaterialBundleModule } from 'src/app/core/material/bundle.module';
 
 @NgModule({
-  declarations: [
-    LoginComponent
-  ],
+  declarations: [LoginComponent],
   imports: [
     CommonModule,
-    LoginRoutingModule
-  ]
+    LoginRoutingModule,
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialBundleModule,
+  ],
 })
-export class LoginModule { }
+export class LoginModule {}
