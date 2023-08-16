ef8b5d0d3806303e443cda6b16de3dca1b0a70b4
Caio Domingos <caiod.dev@gmail.com>
2023-08-16 16:56:18 -0300

    Atualizando dist

--- pre-build.sh
+++ pre-build.sh
@@ -65,11 +65,3 @@ echo docker buildx build --push --platform ${ARCHITECTURE} -t $DOCKERHUB_USER/$F
 end_time=$(date +%s)
 duration=$((end_time - start_time))
 check_success $? $duration
-
-echo -n "Enviando a imagem do frontend para o DockerHub..."
-start_time=$(date +%s)
-echo docker push $DOCKERHUB_USER/$FRONTEND_DIR
-docker push $DOCKERHUB_USER/$FRONTEND_DIR >/dev/null 2>&1 & spinner
-end_time=$(date +%s)
-duration=$((end_time - start_time))
-check_success $? $duration
